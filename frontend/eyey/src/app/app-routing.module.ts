13bea6d9f7a49317c084c820dba035700b2631dc
Isamiasaki <lebendelangeweile@gmail.com>
2022-05-27 21:44:20 +0200

    generated some components

--- frontend/eyey/src/app/app-routing.module.ts
+++ frontend/eyey/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GlossaryComponent } from './components/glossary/glossary.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'glossary', component: GlossaryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
