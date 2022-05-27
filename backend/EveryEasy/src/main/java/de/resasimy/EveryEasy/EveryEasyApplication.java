c91e766066515cca5de95e0b8900fda3a37c6938
RE2SK <re2sk@protonmail.com>
2022-05-27 17:06:14 +0200

    added java project

---
+++ backend/EveryEasy/src/main/java/de/resasimy/EveryEasy/EveryEasyApplication.java
@@ -0,0 +1,13 @@
+package de.resasimy.EveryEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryEasyApplication.class, args);
+	}
+
+}
---
+++ backend/EveryEasy/src/test/java/de/resasimy/EveryEasy/EveryEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.resasimy.EveryEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
