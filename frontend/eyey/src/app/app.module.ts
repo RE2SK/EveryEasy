eb95b92f1a26f7865eb5019b132999bde4704521
Isamiasaki <lebendelangeweile@gmail.com>
2022-05-27 21:44:57 +0200

    [U-Nummer]

--- frontend/eyey/src/app/app.module.ts
+++ frontend/eyey/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatCardModule } from '@angular/material/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { GlossaryComponent } from './components/glossary/glossary.component';
+import { ElementComponent } from './components/element/element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    GlossaryComponent,
+    ElementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
